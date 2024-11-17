09725e784b9435be4bbfb4e075d54d8be03331d8
wyp <wyp@shmtu.edu.cn>
2024-11-17 23:38:08 +0800

    add multiple models

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
+import pathlib
 
 _ = load_dotenv()
 
+model_paths = {}
+model_base_path = os.get('MODEL_BASE_PATH')
 # Load the TAO-8K model and tokenizer (make sure the model is available on Hugging Face or local path)
 # Replace 'tao-8k-model-name' with the actual model identifier or path to your model
 #tokenizer = AutoTokenizer.from_pretrained(os.environ.get('MODEL_PATH'))
-model = SentenceTransformer(os.environ.get('MODEL_PATH'))
+def load_model(model_name: str):
+    model = model_paths.get(model_name)
+    if model is None:
+        path = os.path.join(model_base_path, model_name)
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Model path {path} does not exist.")
+        model = SentenceTransformer(path)
+        model_paths[model_name] = model
+    
+    return model
 
 # Initialize FastAPI app
 app = FastAPI()
@@ -29,27 +41,22 @@ class EmbeddingResponse(BaseModel):
 
 @app.post("/v1/embeddings")
 async def get_embeddings(request: EmbeddingRequest):
-    if request.model != "tao-8k":
-        return {"error": "Unsupported model"}  # Ensure the requested model is 'tao-8k-model'
+    try:
+        model = load_model(request.model)
 
-    # Generate embeddings for each input text
-    
-    embeddings = model.encode(request.input, normalize_embeddings=True)
-    # for text in request.input:
-    #     print(type(text))
-    #     # Tokenize the input text
-    #     embedding = model.encode(text, normalize_embeddings=True)
-    #     embeddings.append({"embedding": embedding.tolist(), "index": len(embeddings)})
-    ret = []
-    for i, embedding in enumerate(embeddings):
-        print(f'i: {i}')
-        obj = {
-            "object": "embedding",
-            "index": i,
-            "embedding": embedding.tolist()
-        }
-        ret.append(obj)
-    
+        # Generate embeddings for each input text
+        embeddings = model.encode(request.input, normalize_embeddings=True)
+        ret = []
+        for i, embedding in enumerate(embeddings):
+            print(f'i: {i}')
+            obj = {
+                "object": "embedding",
+                "index": i,
+                "embedding": embedding.tolist()
+            }
+            ret.append(obj)
 
-    # Return the OpenAI-like response
-    return EmbeddingResponse(data=ret, model=request.model)
+        # Return the OpenAI-like response
+        return EmbeddingResponse(data=ret, model=request.model)
+    except Exception as e:
+        return {"error": str(e)}
