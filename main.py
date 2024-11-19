2d5157e4ee988c2721b5afbb1f3bfdb758417d23
WANG Yuping <wyp@shmtu.edu.cn>
2024-11-19 09:48:37 +0800

    fix bugs

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pathlib
 _ = load_dotenv()
 
 model_paths = {}
-model_base_path = os.get('MODEL_BASE_PATH')
+model_base_path = os.getenv('MODEL_BASE_PATH')
 # Load the TAO-8K model and tokenizer (make sure the model is available on Hugging Face or local path)
 # Replace 'tao-8k-model-name' with the actual model identifier or path to your model
 #tokenizer = AutoTokenizer.from_pretrained(os.environ.get('MODEL_PATH'))
